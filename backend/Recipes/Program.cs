27e5bd0f3295f5c068c3017ff9b6ea53aaab3fff
Philippe ANSELME-VATIN <pavtocode@gmail.com>
2024-09-16 22:31:13 -0400

    test

--- backend/Recipes/Program.cs
+++ backend/Recipes/Program.cs
@@ -3,6 +3,7 @@ using WhatsForDinnerTonight;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
