fcdd57135aed6e3e5c09cab7ce8185e4ff7ece64
Philippe ANSELME-VATIN <pavtocode@gmail.com>
2024-09-22 09:15:23 -0400

    test to connect api. only by NodePort for now :(

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,23 +22,52 @@ const App: React.FC = () => {
   const [showDetails, setShowDetails] = useState(false);
   const [recipes, setRecipes] = useState<{ name: string; ingredients: string[] }[]>([]);
 
- // Function to fetch recipes from the REST service
- const fetchRecipes = async () => {
-  try {
-    // const response = await axios.get('http://localhost:32333/recipes'); // Replace with your REST service URL
-    // const response = await axios.get('http://recipes-clusterip-srv:80/recipes'); // Replace with your REST service URL
-    const response = await axios.get('http://wfdt.com/recipes'); // Replace with your REST service URL
-    console.log(response);
-    setRecipes(response.data);
-  } catch (error) {
-    console.error('Error fetching recipes:', error);
-  }
-};
+  const clusterUrl = 'http://recipes-clusterip-srv/recipes';
+  const localUrl = 'http://localhost:3000/recipes';
+  const wfdtUrl = 'http://wfdt.com:30080/recipes';
 
-// Use useEffect to fetch recipes when the component mounts
-useEffect(() => {
-  fetchRecipes();
-}, []);
+  // Function to fetch recipes from the REST service
+  // const fetchRecipesLocal = async () => {
+  //   try {
+  //     console.log(localUrl);
+  //     const response = await axios.get(localUrl); // Replace with your REST service URL
+  //     setRecipes(response.data);
+  //   } catch (error) {
+  //     console.error('Error fetching recipes:', error);
+  //   }
+  // }
+
+
+  // const fetchRecipes = async () => {
+  //   try {
+  //     // const response = await axios.get('http://localhost:32333/recipes'); // Replace with your REST service URL
+  //     console.log(clusterUrl);
+  //     const response = await axios.get(clusterUrl); // Replace with your REST service URL
+  //     setRecipes(response.data);
+  //   } catch (error) {
+  //     console.error('Error fetching recipes:', error);
+  //   }
+  // }
+    const fetchRecipesWfdt = async () => {
+      try {
+        console.log(wfdtUrl);
+        const responseCluster = await axios.get(wfdtUrl); // Replace with your REST service URL
+        console.log(responseCluster);
+        setRecipes(responseCluster.data);
+      
+      
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+      }
+  
+  };
+
+  // Use useEffect to fetch recipes when the component mounts
+  useEffect(() => {
+    // fetchRecipes();
+    fetchRecipesWfdt();
+    // fetchRecipesLocal();
+  }, []);
 
 
   // Function to select a random recipe
@@ -57,22 +86,22 @@ useEffect(() => {
     // <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
     <div>
       <Container maxWidth="sm">
-      <Box sx={{ width: '100%' }} style={{alignContent: "center"}} alignItems= 'center'>
-        <Stack spacing={0.5} divider={<Divider orientation="vertical" flexItem />}>
-          <ListItem>
-            <RecipeButton onClick={handleSelectRecipe} />
-          </ListItem>
+        <Box sx={{ width: '100%' }} style={{ alignContent: "center" }} alignItems='center'>
+          <Stack spacing={0.5} divider={<Divider orientation="vertical" flexItem />}>
+            <ListItem>
+              <RecipeButton onClick={handleSelectRecipe} />
+            </ListItem>
           </Stack>
-      </Box>
+        </Box>
       </Container>
       <Container maxWidth="sm">
-      <Box sx={{ width: '100%' }} style={{alignContent: "center"}} alignItems= 'center'>
-        <Stack spacing={0.5} divider={<Divider orientation="vertical" flexItem />}>
-          <ListItem>
-            <RecipeDisplay selectedRecipe={selectedRecipe} showDetails={showDetails} onShowDetails={handleShowDetails} />
-          </ListItem>
-        </Stack>
-      </Box>
+        <Box sx={{ width: '100%' }} style={{ alignContent: "center" }} alignItems='center'>
+          <Stack spacing={0.5} divider={<Divider orientation="vertical" flexItem />}>
+            <ListItem>
+              <RecipeDisplay selectedRecipe={selectedRecipe} showDetails={showDetails} onShowDetails={handleShowDetails} />
+            </ListItem>
+          </Stack>
+        </Box>
       </Container>
     </div>
   );
